1148e3edc35d1c61fc716347b3d0fc2af52a34c8
Lucas Q <lucas.14.96@gmail.com>
2021-04-30 17:46:45 -0500

    Recreated entire website, mobile responsiveness pending

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 250);
+
+	/* JQuery Menu
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('header nav').meanmenu();
+	});
+
+
+
+});
